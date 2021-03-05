7e542ccb45eede67724b3ef8240ec00e35952503
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-03-05 14:53:45 -0800

    --wip-- [skip ci]

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,15 @@ module.exports = {
   env: {
     browser: true,
   },
-  rules: {},
+  rules: {
+    'ember/no-get': 0,
+    'ember/no-new-mixins': 0,
+    'ember/no-mixins': 0,
+    'ember/no-classic-components': 0,
+    'ember/no-classic-classes': 0,
+    'ember/no-actions-hash': 0,
+    'ember/require-tagless-components': 0,
+  },
   overrides: [
     // node files
     {
